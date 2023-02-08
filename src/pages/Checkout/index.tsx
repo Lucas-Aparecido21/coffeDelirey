0b7bad8d1151744e617356dac8c0c955aabad3b4
EquipeWeb02 <equipeweb02@Mac-Filipe.local>
2023-02-08 17:03:11 -0200

    localstorage removed

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -39,7 +39,21 @@ import {
 import { CartListCheckout } from "./Components/Cart";
 import { NavLink } from "react-router-dom";
 import { useCart } from "../../hooks/useCart";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import api from "../../services/api";
+
+interface Cliente {
+  cpf: string;
+  nome: string;
+  telefone: string;
+  cep: string;
+  rua: string;
+  cidade: string;
+  bairro: string;
+  uf: string;
+  numero: string;
+  complemento?: string;
+}
 
 export function Checkout() {
   let { setFormPag, formPag } = useCart();
@@ -48,140 +62,54 @@ export function Checkout() {
   const { cartItems, cartItemsTotal } = useCart();
   const cartTotal = DELIVERY_PRICE + cartItemsTotal;
 
-  const savedName = localStorage.getItem("inputName");
-  const savedSName = localStorage.getItem("inputSName");
-  const savedCel = localStorage.getItem("inputCel");
-  const savedCEP = localStorage.getItem("inputValue");
-  const savedLogradouro = localStorage.getItem("inputLogra");
-  const savedBairro = localStorage.getItem("inputBairro");
-  const savedComplemento = localStorage.getItem("inputComplemento");
-  const savedUf = localStorage.getItem("inputUf");
-  const savedNumber = localStorage.getItem("inputNumber");
-  const savedLocalidade = localStorage.getItem("inputLocalidade");
-
   const { setCartItems } = useCart();
 
-  const [inputName, setInputName] = useState(savedName || "");
-  const [inputSName, setInputSName] = useState(savedSName || "");
-  const [inputCel, setInputCel] = useState(savedCel || "");
+  const [cliente, setCliente] = useState<Cliente>({} as Cliente);
   const [valueNav, setValueNav] = useState("/Checkout");
+  // const ConsultaCEP = (event: any) => {
+  //   const cep = event.target.value;
 
-  const [inputValue, setInputValue] = useState(savedCEP || "");
-  const [inputNumber, setInputNumber] = useState(savedNumber || "");
-  const [inputComplemento, setInputComplemento] = useState(
-    savedComplemento || ""
-  );
-  const [inputLogradouro, setInputLogradouro] = useState(savedLogradouro || "");
-  const [inputBairro, setInputBairro] = useState(savedBairro || "");
-  const [inputLocalidade, setInputLocalidade] = useState(savedLocalidade || "");
-  const [inputUf, setInputUf] = useState(savedUf || "");
+  //   fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setInputLogradouro(data.logradouro);
+  //       setInputBairro(data.bairro);
+  //       setInputLocalidade(data.localidade);
+  //       setInputUf(data.uf);
+  //     });
 
-  const ConsultaCEP = (event: any) => {
-    const cep = event.target.value;
-
-    fetch(`https://viacep.com.br/ws/${cep}/json/`)
-      .then((res) => res.json())
-      .then((data) => {
-        setInputLogradouro(data.logradouro);
-        setInputBairro(data.bairro);
-        setInputLocalidade(data.localidade);
-        setInputUf(data.uf);
-      });
-
-    localStorage.setItem("inputValue", inputValue);
-
-    useEffect(() => {
-      const savedCEP = localStorage.getItem("inputValue");
-      if (savedCEP !== null) {
-        setInputValue(savedCEP);
-      }
-    }, []);
+  const ConsultaCPF = (event: any) => {
+    const cpf2 = event.target.value;
+    console.log(cpf2);
+    api.get(`clientes/${cpf2}`).then((res) => {});
   };
 
-  function saveToLocalStorageLogradouro() {
-    localStorage.setItem("inputLogradouro", inputLogradouro);
-  }
+  console.log(ConsultaCPF);
 
-  useEffect(() => {
-    const savedLogradouro = localStorage.getItem("inputLogradouro");
-    if (savedLogradouro !== null) {
-      setInputLogradouro(savedLogradouro);
-    }
-  }, []);
-
-  function saveToLocalStorageNumber() {
-    localStorage.setItem("inputNumber", inputNumber);
-  }
-
-  useEffect(() => {
-    const savedNumber = localStorage.getItem("inputNumber");
-    if (savedNumber !== null) {
-      setInputNumber(savedNumber);
-    }
-  }, []);
-
-  function saveToLocalStorageComplemento() {
-    localStorage.setItem("inputComplemento", inputComplemento);
-  }
-
-  useEffect(() => {
-    const savedComplemento = localStorage.getItem("inputComplemento");
-    if (savedComplemento !== null) {
-      setInputComplemento(savedComplemento);
-    }
-  }, []);
-
-  function saveToLocalStorageBairro() {
-    localStorage.setItem("inputBairro", inputBairro);
-  }
-
-  useEffect(() => {
-    const savedBairro = localStorage.getItem("inputBairro");
-    if (savedBairro !== null) {
-      setInputBairro(savedBairro);
-    }
-  }, []);
-
-  function saveToLocalStorageLocalidade() {
-    localStorage.setItem("inputLocalidade", inputLocalidade);
-  }
-
-  useEffect(() => {
-    const savedLocalidade = localStorage.getItem("inputLocalidade");
-    if (savedLocalidade !== null) {
-      setInputLocalidade(savedLocalidade);
-    }
-  }, []);
-
-  function saveToLocalStorageUf() {
-    localStorage.setItem("inputUf", inputUf);
-  }
-
-  useEffect(() => {
-    const savedUf = localStorage.getItem("inputUf");
-    if (savedUf !== null) {
-      setInputUf(savedUf);
-    }
-  }, []);
+  // const changeCliente = (campo: keyof typeof cliente, valor: string) => {
+  //   setCliente((prevState) => {
+  //     return { ...prevState, [campo]: valor }
+  //   })
+  // }
 
   function handleClick() {
-    if (inputValue === "") {
+    if (cliente.rua === "") {
       alert("O Endereço de entrega é obrigatório");
       return;
     }
-    if (inputNumber === "") {
+    if (cliente.numero === "") {
       alert("O Numero do endereço é obrigatório");
       return;
     }
-    if (inputName === "") {
+    if (cliente.cpf === "") {
       alert("As Informações pessoais são obrigatórias");
       return;
     }
-    if (inputSName === "") {
+    if (cliente.nome === "") {
       alert("As Informações pessoais são obrigatórias");
       return;
     }
-    if (inputCel === "") {
+    if (cliente.telefone === "") {
       alert("As Informações pessoais são obrigatórias");
       return;
     }
@@ -194,37 +122,6 @@ export function Checkout() {
     }
   }
 
-  function saveToLocalStorageCel() {
-    localStorage.setItem("inputCel", inputCel);
-  }
-  useEffect(() => {
-    const savedCel = localStorage.getItem("inputCel");
-    if (savedCel !== null) {
-      setInputCel(savedCel);
-    }
-  }, []);
-
-  function saveToLocalStorageSName() {
-    localStorage.setItem("inputSName", inputSName);
-  }
-
-  useEffect(() => {
-    const savedName = localStorage.getItem("inputName");
-    if (savedName !== null) {
-      setInputName(savedName);
-    }
-  }, []);
-
-  function saveToLocalStorageName() {
-    localStorage.setItem("inputName", inputName);
-  }
-
-  useEffect(() => {
-    const savedName = localStorage.getItem("inputName");
-    if (savedName !== null) {
-      setInputName(savedName);
-    }
-  }, []);
   return (
     <>
       <Header />
@@ -245,23 +142,20 @@ export function Checkout() {
                 <DivName>
                   <InputNome
                     placeholder="CPF"
-                    value={inputName}
-                    onChange={(e) => setInputName(e.target.value)}
-                    onBlur={saveToLocalStorageName}
+                    value={cliente.cpf}
+                    // onChange={(e) => setinputCPF(e.target.value)}
                   />
                   <InputSNome
                     placeholder="Nome"
-                    value={inputSName}
-                    onChange={(e) => setInputSName(e.target.value)}
-                    onBlur={saveToLocalStorageSName}
+                    value={cliente.nome}
+                    // onChange={(e) => setInputSName(e.target.value)}
                   />
                 </DivName>
                 <InputCel
                   placeholder="Celular"
                   type="tel"
-                  value={inputCel}
-                  onChange={(e) => setInputCel(e.target.value)}
-                  onBlur={saveToLocalStorageCel}
+                  value={cliente.telefone}
+                  // onChange={(e) => setInputCel(e.target.value)}
                 />
               </form>
             </DivInfo>
@@ -277,68 +171,62 @@ export function Checkout() {
                 Informe o endereço de entrega onde deseja receber seu pedido
               </p>
 
-
               <form>
                 <CepInput
                   placeholder="CEP"
                   type="text"
-                  onBlur={ConsultaCEP}
-                  value={inputValue}
-                  onChange={(e) => setInputValue(e.target.value)}
+                  // onBlur={ConsultaCEP}
+                  value={cliente.cep}
+                  // onChange={(e) => setInputValue(e.target.value)}
                 ></CepInput>
 
                 <RuaInput
                   placeholder="Rua"
                   type="text"
-                  value={inputLogradouro}
-                  // {...register("logradouro")}
-                  onBlur={saveToLocalStorageLogradouro}
-                  onChange={(e) => setInputLogradouro(e.target.value)}
+                  value={cliente.rua}
+                  // onChange={(e) => setInputLogradouro(e.target.value)}
                 ></RuaInput>
                 <div className="Separador1">
                   <NumeroInput
                     placeholder="Número"
                     type="text"
-                    value={inputNumber}
-                    onBlur={saveToLocalStorageNumber}
-                    onChange={(e) => setInputNumber(e.target.value)}
+                    value={cliente.numero}
+                    // onChange={(e) => setInputNumber(e.target.value)}
                   ></NumeroInput>
                   <ComplementoInput
                     placeholder="Complemento (opcional)"
                     type="text"
-                    value={inputComplemento}
-                    onBlur={saveToLocalStorageComplemento}
-                    onChange={(e) => setInputComplemento(e.target.value)}
+                    value={cliente.complemento}
+                    // onChange={(e) => setInputComplemento(e.target.value)}
                   ></ComplementoInput>
                 </div>
                 <div className="Separador2">
                   <BairroInput
                     placeholder="Bairro"
                     type="text"
-                    value={inputBairro}
-                    onBlur={saveToLocalStorageBairro}
-                    onChange={(e) => setInputBairro(e.target.value)}
+                    value={cliente.bairro}
+
+                    // onChange={(e) => setInputBairro(e.target.value)}
                   ></BairroInput>
                   <CidadeInput
                     placeholder="Cidade"
                     type="text"
-                    value={inputLocalidade}
-                    onBlur={saveToLocalStorageLocalidade}
-                    onChange={(e) => setInputLocalidade(e.target.value)}
+                    value={cliente.cidade}
+
+                    // onChange={(e) => setInputLocalidade(e.target.value)}
                   ></CidadeInput>
                   <UFInput
                     placeholder="UF"
                     type="text"
-                    value={inputUf}
-                    onBlur={saveToLocalStorageUf}
-                    onChange={(e) => setInputUf(e.target.value)}
+                    value={cliente.uf}
+
+                    // onChange={(e) => setInputUf(e.target.value)}
                   ></UFInput>
                 </div>
               </form>
             </div>
           </ContainerCep>
 
-         
           <ContainerFpg>
             <DivFpg>
               <div>
